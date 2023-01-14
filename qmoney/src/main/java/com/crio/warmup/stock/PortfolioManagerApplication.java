152cb349f27501cc5255a24a9ddcf7362fc400e1
CrioUser <crio-user@criodo.com>
2023-01-15 03:50:17 +0530

    ME_QMONEY_V2_MODULE_REST_API

--- qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
+++ qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
@@ -139,16 +139,21 @@ return tests;
   //  After refactor, make sure that the tests pass by using these two commands
   //  ./gradlew test --tests PortfolioManagerApplicationTest.readTradesFromJson
   //  ./gradlew test --tests PortfolioManagerApplicationTest.mainReadFile
- /*  public static List<PortfolioTrade> readTradesFromJson(String filename) throws IOException, URISyntaxException {
-     return Collections.emptyList();
-  }*/
+   public static List<PortfolioTrade> readTradesFromJson(String filename) throws IOException, URISyntaxException {
+    ObjectMapper objectMapper = getObjectMapper();
+    return  Arrays.asList(objectMapper.readValue(resolveFileFromResources(filename), PortfolioTrade[].class));
+  }
 
 
   // TODO:
   //  Build the Url using given parameters and use this function in your code to cann the API.
- /*  public static String prepareUrl(PortfolioTrade trade, LocalDate endDate, String token) {
-     return Collections.emptyList();
-  }*/
+   public static String prepareUrl(PortfolioTrade trade, LocalDate endDate, String token) {
+     //return Collections.emptyList();
+     String url = "https://api.tiingo.com/tiingo/daily/" + trade.getSymbol() + "/prices?startDate=" 
+        + trade.getPurchaseDate().toString() + "&endDate=" + endDate
+        + "&token=token";
+    return url;
+  }
 
   
   
